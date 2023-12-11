22f2355cd732baef0f8c294d5c4ec01eaa07a10c
albim72 <marcin.albiniak72@gmail.com>
2023-12-11 15:06:01 +0100

    Update main.py

--- DZIEN_1/PROJEKt_OSOBA/main.py
+++ DZIEN_1/PROJEKt_OSOBA/main.py
@@ -1,6 +1,8 @@
 from osoba import Osoba
 
 os1 = Osoba("Jan",56,88,178)
+os1.setoczy("niebieskie")
+print(f"zmieniono kolor oczu na  :{os1.getoczy()} ")
 print(os1)
 n=12
 print(f'wiek osoby za {n} lat -> {os1.wiek_za_n_lat(n)}')
