711f0cb4deebbb1d206c7a660291a39e05ddd7f2
albim72 <marcin.albiniak72@gmail.com>
2023-12-11 15:20:01 +0100

    Update main.py

--- DZIEN_1/PROJEKt_OSOBA/main.py
+++ DZIEN_1/PROJEKt_OSOBA/main.py
@@ -1,4 +1,5 @@
 from osoba import Osoba
+from pracownik import Pracownik
 
 os1 = Osoba("Jan",56,88,178)
 os1.setoczy("niebieskie")
@@ -9,3 +10,10 @@ print(f'wiek osoby za {n} lat -> {os1.wiek_za_n_lat(n)}')
 print(f'czy osoba jest pracownikiem? ({os1.czypracownik()})')
 
 print("_"*50)
+
+pr1 = Pracownik("Karol",50,80,180,"ABC","dyrektor",12,11200)
+pr1.setoczy("piwne")
+print(pr1)
+n=5
+print(f'wiek pracownika za {n} lat -> {pr1.wiek_za_n_lat(n)}')
+print(f'czy osoba jest pracownikiem? ({pr1.czypracownik()})')
