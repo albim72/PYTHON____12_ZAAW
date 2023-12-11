061e1d4141a016e335596c52bea2d022c916e5f1
albim72 <marcin.albiniak72@gmail.com>
2023-12-11 11:09:54 +0100

    Update funkcja_w_funkcji.py

--- DZIEN_1/funkcja_w_funkcji.py
+++ DZIEN_1/funkcja_w_funkcji.py
@@ -43,3 +43,45 @@ def rejestracja(oplata):
 print(rejestracja(1)(45))
 print(rejestracja(0)())
 print(rejestracja(15)())
+
+
+#przykład 3
+def startstop(funkcja):
+    def wrapper(*args):
+        print("*"*50)
+        print("startowanie procesu....")
+        funkcja(*args)
+        print("zakończenie procesu...")
+    return wrapper
+
+def zawijanie(w_co):
+    print(f'zawijanie czekoladek w {w_co}')
+
+zw = startstop(zawijanie)
+zw("sreberka")
+
+@startstop
+def dmuchanie(czego):
+    print(f'dmuchanie {czego} na torcie urodzinowym.')
+
+
+dmuchanie("świeczek")
+
+@startstop
+def fx(n):
+    print(f'wynik = {2*n-1}')
+
+fx(9)
+
+#standardowe funkcje wyższego rzędu
+liczby = [45,12,78,90,-23,0,34,56,78,111,-45,11,5,-3,100,-133,98,25,-4,4]
+
+parzyste = list(filter(lambda x:x%2==0,liczby))
+print(parzyste)
+
+cube = list(map(lambda x:x**3,liczby))
+print(cube)
+
+#list comprhension
+dane = [i**2 for i in range(120)]
+print(dane)
