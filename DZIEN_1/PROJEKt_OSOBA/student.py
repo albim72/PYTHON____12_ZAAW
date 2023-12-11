d079f02edeb2ac2cd63c6e7350a7fcd502d9b248
albim72 <marcin.albiniak72@gmail.com>
2023-12-11 15:56:08 +0100

    Update student.py

--- DZIEN_1/PROJEKt_OSOBA/student.py
+++ DZIEN_1/PROJEKt_OSOBA/student.py
@@ -6,18 +6,25 @@ from ekstra import Ekstra
 class Student(Pracownik,Sport,Ekstra):
 
     def __init__(self, imie, wiek, waga, wzrost, id_studenta, wydzial, kierunek, rok_stud,
-                 firma="", stanowisko="", lata_pracy="", wynagrodzenie="", 
-                 dysycyplina="", lata_upr="", best_wynik=""):
+                 firma="", stanowisko="", lata_pracy="", wynagrodzenie="",
+                 dyscyplina="", lata_upr="", best_wynik=""):
         Pracownik.__init__(self,imie, wiek, waga, wzrost, firma, stanowisko, lata_pracy, wynagrodzenie)
-        Sport.__init__(self,dysycyplina,lata_upr,best_wynik)
+        Sport.__init__(self,dyscyplina,lata_upr,best_wynik)
         self.id_studenta = id_studenta
         self.wydzial = wydzial
         self.kierunek = kierunek
         self.rok_stud = rok_stud
-        
+
     def __repr__(self):
         return (f'student nr {self.id_studenta}, wydział: {self.wydzial},'
                 f'kierunek: {self.kierunek}. rok studiów: {self.rok_stud}')
 
     def czypracownik(self):
         return self.firma != ""
+
+
+
+
+
+
+
