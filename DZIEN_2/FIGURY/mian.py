fbc894a245b7c8515f0f143f576b2ba897e45eb5
albim72 <marcin.albiniak72@gmail.com>
2023-12-12 09:39:04 +0100

    Update mian.py

--- DZIEN_2/FIGURY/mian.py
+++ DZIEN_2/FIGURY/mian.py
@@ -1,5 +1,15 @@
 from trojkat import Trojkat
+from prostokat import Prostokat
 
 tr = Trojkat(5.6,6.8)
 print(f'pole figury {tr.__class__.__name__} wynosi: {tr.policz_pole():.2f} cm2')
+print("_"*50)
+
+pr1 = Prostokat(4,4.7)
+print(f'pole figury {pr1.__class__.__name__} wynosi: {pr1.policz_pole():.2f} cm2')
+print("_"*50)
+
+pr2 = Prostokat(4,4)
+print(f'pole figury {pr2.__class__.__name__} wynosi: {pr2.policz_pole():.2f} cm2')
+print(type(pr2))
 print("_"*50)
