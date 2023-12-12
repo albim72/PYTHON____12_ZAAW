ce6b4651b9ab44f3e6cf8673890736700ec49d91
albim72 <marcin.albiniak72@gmail.com>
2023-12-12 10:21:42 +0100

    Update and rename mian.py to main.py

--- DZIEN_2/FIGURY/main.py
+++ DZIEN_2/FIGURY/main.py
@@ -1,5 +1,7 @@
 from trojkat import Trojkat
 from prostokat import Prostokat
+from trapez import Trapez
+from Kolo import Kolo
 
 tr = Trojkat(5.6,6.8)
 print(f'pole figury {tr.__class__.__name__} wynosi: {tr.policz_pole():.2f} cm2')
@@ -12,4 +14,12 @@ print("_"*50)
 pr2 = Prostokat(4,4)
 print(f'pole figury {pr2.__class__.__name__} wynosi: {pr2.policz_pole():.2f} cm2')
 print(type(pr2))
+print("_"*50)
+
+trp = Trapez(8.2,6.3,4.4)
+print(f'pole figury {trp.__class__.__name__} wynosi: {trp.policz_pole():.2f} cm2')
+print("_"*50)
+
+kl = Kolo(5.5)
+print(f'pole figury {kl.__class__.__name__} wynosi: {kl.policz_pole():.2f} cm2')
 print("_"*50)
