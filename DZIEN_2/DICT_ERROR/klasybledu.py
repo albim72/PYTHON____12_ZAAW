754a14d9b507cf8c84b84f6294f19f7f40d3b6f8
albim72 <marcin.albiniak72@gmail.com>
2023-12-12 13:24:39 +0100

    Create klasybledu.py

---
+++ DZIEN_2/DICT_ERROR/klasybledu.py
@@ -0,0 +1,18 @@
+class IntFloatValueError(Exception):
+    def __init__(self,value):
+        self.value = value
+        
+    def __str__(self):
+        return (f'wartość {self.value} jest niewłaściwa!'
+                f'Akceptowalne są tylko wartości w typie int oraz float.')
+    
+    
+class KeyValueConstructError(Exception):
+    def __init__(self,key,value):
+        self.key = key
+        self.value = value
+        
+    def __str__(self):
+        return (f"klucze i wartości winny być przekazane w listach lub krotkach!"
+                f" klucz: {self.key} jest w typie: {type(self.key)}"
+                f" wartość: {self.value} jest w typie: {type(self.value)}")
