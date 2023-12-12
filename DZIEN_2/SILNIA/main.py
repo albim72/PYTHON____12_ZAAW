d91e9f46d0fb6acbca0a7a1579711442b8df3df0
albim72 <marcin.albiniak72@gmail.com>
2023-12-12 14:05:21 +0100

    Update main.py

--- DZIEN_2/SILNIA/main.py
+++ DZIEN_2/SILNIA/main.py
@@ -6,6 +6,7 @@ import sys
 from liczbujemna import UjemneN
 
 sys.set_int_max_str_digits(0x10000000)
+sys.setrecursionlimit(0x10000000)
 def silnia(n):
     if n<0:
         raise UjemneN(n)
@@ -14,11 +15,22 @@ def silnia(n):
         wynik*=i
     return wynik
 
+def silnia_rek(n):
+    if n<0:
+        raise UjemneN(n)
+    if n==0:
+        return 1
+    else:
+        return n*silnia_rek(n-1)
+
 try:
     n = int(input("podaj wartość argumentu n funkcji silnia: "))
     wynik = silnia(n)
+    wynik_r = silnia_rek(n)
     print(f'silnia z n={n} wynosi: {wynik}')
+    print(f'silnia rekurencyjna z n={n} wynosi: {wynik_r}')
     print(f'typ wyniku: {type(wynik)}')
+    print(f'typ wyniku rek: {type(wynik_r)}')
 except UjemneN as un:
     print(un)
 except ValueError as ve:
