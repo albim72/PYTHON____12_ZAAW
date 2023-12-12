fc307312e76e350e5b3cce40eacd30b85aa24f50
albim72 <marcin.albiniak72@gmail.com>
2023-12-12 08:36:27 +0100

    Update main.py

--- DZIEN_2/main.py
+++ DZIEN_2/main.py
@@ -1,5 +1,6 @@
 from  oldresistor import OldResistor
 from resistor import Resistor
+from voltage import VoltageResistance
 
 print("________________ klasa OldResistor _______________")
 r0 = OldResistor(10.2E2)
@@ -13,3 +14,10 @@ r1 = Resistor(50E3)
 r1.ohms = 18.3
 print(f'układ elektryczny:\noporność: {r1.ohms} omów\nnapięcie prądu: {r1.voltage} V\n'
       f'natężenie prądu: {r1.current} A\n')
+
+print("________________ klasa VoltageResistance _______________")
+r2 = VoltageResistance(1E3)
+print(f'początkowe natężenie prądu: {r2.current} A')
+r2.voltage = 39
+print(f'napięcie prądu: {r2.voltage} V')
+print(f'natężenie prądu: {r2.current} A')
