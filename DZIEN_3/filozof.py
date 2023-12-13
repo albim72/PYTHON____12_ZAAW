b381b0f03ffca27c95aed0427fdcddb600b7a346
albim72 <marcin.albiniak72@gmail.com>
2023-12-13 09:50:57 +0100

    Update filozof.py

--- DZIEN_3/filozof.py
+++ DZIEN_3/filozof.py
@@ -7,13 +7,19 @@ else:
 def odpowiedz(self):
     return "Tak! Ziemia jest płaska!"
 
+def odpowiedz_nowa(self):
+    return "Nie! Ziemia jest elipsoidą!"
+
 def brak(self):
     return "brak odpowiedzi..."
 
 class SednoOdpowiedzi(type):
     def __init__(cls,clsname,bases,attrs):
         if required:
-            cls.odpowiedz = odpowiedz
+            if clsname == "Kopernik":
+                cls.odpowiedz = odpowiedz_nowa
+            else:
+                cls.odpowiedz = odpowiedz
         else:
             cls.odpowiedz = brak
 
@@ -42,3 +48,6 @@ print(f'Filozof {fill3.__class__.__name__} twierdzi: {fill3.odpowiedz()}')
 
 fill4 = Kopernik()
 print(f'Filozof {fill4.__class__.__name__} twierdzi: {fill4.odpowiedz()}')
+
+#Skonstruuj rozwiązanie pozwalające Kopernikowi na wypowiedź: Nie! Ziemia jest elipsoidą!
+#Pozostaw aktualną konstrukcję.
