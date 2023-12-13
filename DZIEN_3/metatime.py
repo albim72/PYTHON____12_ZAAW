a5dab7deb850d82e3cf0a74d5e8aacd044b95326
albim72 <marcin.albiniak72@gmail.com>
2023-12-13 11:50:20 +0100

    Update metatime.py

--- DZIEN_3/metatime.py
+++ DZIEN_3/metatime.py
@@ -6,39 +6,50 @@ class Timer:
         self.elapsed = 0.0
         self._func = func
         self._start = None
-        
+
     def start(self):
         if self._start is not None:
             raise RuntimeError('już działa!')
         self._start = self._func()
-        
+
     def stop(self):
         if self._start is None:
             raise RuntimeError('funkcja nie została uruchomiona!')
         end = self._func()
         self.elapsed += end - self._start
         self._start = None
-        
+
     def reset(self):
         self.elapsed = 0.0
-        
+
     @property
     def running(self):
         return self._start is not None
-    
+
     def __enter__(self):
         self.start()
         return self
-    
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
         
+        
+def timefunc(fn,*args,**kwargs):
+    def fncomposite(*args,**kwargs):
+        timer = Timer()
+        timer.start()
+        rt = fn(*args,**kwargs)
+        timer.stop()
+        print(f'wykonanie {fn.__name__} zajęło {timer.elapsed} s')
+        return rt
+    return fncomposite
+
+
+
 class Timed(type):
     def __new__(cls,clsname,bases,attrs):
         for name,value in attrs.items():
             if type(value) is types.FunctionType or type(value) is types.MethodType:
-                #attrs[name] = 
-                pass
-            
+                attrs[name] = timefunc(value)
+
         return super(Timed,cls).__new__(cls,clsname,bases,attrs)
-        
