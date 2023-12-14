080de8287d5b73d839e296895d91a9af02cf2df3
albim72 <marcin.albiniak72@gmail.com>
2023-12-14 11:08:34 +0100

    Update footballiterator.py

--- DZIEN_4/footballiterator.py
+++ DZIEN_4/footballiterator.py
@@ -2,7 +2,7 @@ class FootballTeamIterator:
     def __init__(self,members):
         self.members = members
         self.index = 0
-        
+
     def __iter__(self):
         return self
 
@@ -13,5 +13,26 @@ class FootballTeamIterator:
             return val
         else:
             raise StopIteration()
-    
-    
+
+class FootballTeam:
+    def __init__(self,members):
+        self.memebers = members
+
+    def __iter__(self):
+        return FootballTeamIterator(self.memebers)
+
+
+def main():
+    members = [f'gracz{str(x)}' for x in range(1,23)]
+    members = members + ['trener1','trener2','trener3','fizjoterapeuta','rzecznik']
+    team = FootballTeam(members)
+    team_it = iter(team)
+    # while True:
+    #     print(next(team_it))
+
+    for member in team:
+        print(member)
+
+
+if __name__ == '__main__':
+    main()
